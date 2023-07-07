fb9ff2a889d6e3a3e1f2cdddee81d5683ef88625
Javi Moreno <email@javier-moreno.com>
2023-07-07 12:52:09 +0200

    Language changed to spanish

--- requests.js
+++ requests.js
@@ -1,14 +1,14 @@
 export const apiKey = process.env.API_KEY;
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${apiKey}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${apiKey}&witg_network=213`,
-  fetchTopRated: `/movie/top_rated?api_key=${apiKey}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movie?api_key=${apiKey}&with_genres=99`,
+  fetchTrending: `/trending/all/week?api_key=${apiKey}&language=es-ES`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${apiKey}&witg_network=213&language=es-ES`,
+  fetchTopRated: `/movie/top_rated?api_key=${apiKey}&language=es-ES`,
+  fetchActionMovies: `/discover/movie?api_key=${apiKey}&with_genres=28&language=es-ES`,
+  fetchComedyMovies: `/discover/movie?api_key=${apiKey}&with_genres=35&language=es-ES`,
+  fetchHorrorMovies: `/discover/movie?api_key=${apiKey}&with_genres=27&language=es-ES`,
+  fetchRomanceMovies: `/discover/movie?api_key=${apiKey}&with_genres=10749&language=es-ES`,
+  fetchDocumentaries: `/discover/movie?api_key=${apiKey}&with_genres=99&language=es-ES`,
 };
 
 export default requests;
